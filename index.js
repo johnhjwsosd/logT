021858d8d9b39027d6c843db03fe6df0c7bf83ec
ll <johnhjwsosd@163.com>
2016-12-05 10:53:42 +0800

    env explain

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ module.exports = function (level, infotitle, logcontent, collectionname) {
 /**
  * test
  */
+//env MONGO_DB_CONFIG   mongodb://127.0.0.1:27017/log
+
  //logger.logwrite("info","test", "this is a test","test");
  //logger.logwrite("error","test", "this is a test","test");
